af73ae1e9d821dc841716e47d68b1e373bba2e16
Ankit Goila <agoila@umich.edu>
2020-08-02 01:54:58 -0400

    Update uniform-distribution-improved.py

--- problems/Probability-Distributions/uniform-distribution-improved.py
+++ problems/Probability-Distributions/uniform-distribution-improved.py
@@ -1,20 +1,16 @@
 def probability_range_improved(low_range, high_range, minimum, maximum):
-
-    # TODO: check if any of the inputs are strings.
-    # hint: the python function isinstance() will be useful
+    
     inputs = [low_range, high_range, minimum, maximum]
     if (any(isinstance(i, str) for i in inputs)):
         # print a message to the user and return none
         print('Inputs should be numbers not string')
         return None
     
-    # TODO check that low_range is between minimum and maximum
     if (low_range < minimum or low_range > maximum):
         # print a message to the user and return none
         print('Your low range value must be between minimum and maximum')
         return None
         
-    # TODO check that high_range is between min and max
     if (high_range < minimum or high_range > maximum):
         # print a message to the user and return none
         print('The high range value must be between minimum and maximum')
