2682297b0cbd78b6cc5cdab3995b77e2d39297f1
Ankit Goila <agoila@umich.edu>
2020-08-02 01:53:57 -0400

    improvements

---
+++ problems/Probability-Distributions/uniform-distribution-improved.py
@@ -0,0 +1,33 @@
+def probability_range_improved(low_range, high_range, minimum, maximum):
+
+    # TODO: check if any of the inputs are strings.
+    # hint: the python function isinstance() will be useful
+    inputs = [low_range, high_range, minimum, maximum]
+    if (any(isinstance(i, str) for i in inputs)):
+        # print a message to the user and return none
+        print('Inputs should be numbers not string')
+        return None
+    
+    # TODO check that low_range is between minimum and maximum
+    if (low_range < minimum or low_range > maximum):
+        # print a message to the user and return none
+        print('Your low range value must be between minimum and maximum')
+        return None
+        
+    # TODO check that high_range is between min and max
+    if (high_range < minimum or high_range > maximum):
+        # print a message to the user and return none
+        print('The high range value must be between minimum and maximum')
+        return None
+
+    # TODO: calulate and return the probability 
+    # even if low range is greater than high range
+    probability = (abs(high_range - low_range)) / (maximum - minimum)
+    return probability
+
+assert probability_range_improved('a', 0, -100, 500) == None
+assert probability_range_improved(0, 'b', -100, 500) == None
+assert probability_range_improved(-100, 300, 100, 500) == None
+assert probability_range_improved(105, 700, 100, 500) == None
+assert "{0:.2f}".format(probability_range_improved(25, 700, 5, 800)) == '0.85'
+assert "{0:.2f}".format(probability_range_improved(700, 25, 5, 800)) == '0.85'
