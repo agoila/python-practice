f65f305107dffe3ee02729ad82581b8e06bcf833
Ankit Goila <agoila@umich.edu>
2020-08-02 01:10:22 -0400

    Update uniform-distribution.py

--- problems/Probability-Distributions/uniform-distribution.py
+++ problems/Probability-Distributions/uniform-distribution.py
@@ -1,10 +1,4 @@
 def probability_uniform(low_range, high_range, minimum, maximum):
-    
-    ## TODO: Calculate the probability of an event occurring 
-    ## between low_range and high_range.
-    ## Assume the user has given valid inputs such that low_range < high_range.
-    ##   minimum < maximum
-    ##
 
     probability = (high_range - low_range) / (maximum - minimum) 
     
@@ -14,4 +8,3 @@ def probability_uniform(low_range, high_range, minimum, maximum):
 assert "{0:.2f}".format(probability_uniform(15, 305, 0, 360)) == '0.81'
 assert "{0:.2f}".format(probability_uniform(1, 5, 0, 10)) == '0.40'
 assert "{0:.2f}".format(probability_uniform(55, 70, 20, 300)) == '0.05'
-print('Great work! Your code outputs the expected results.')
